dba3b9130af15f25af676bd00efeea3041cf890c
ppielka <ppielka@pgs-soft.com>
2023-08-30 16:43:51 +0200

    update main and methods files

--- functions/methods.py
+++ functions/methods.py
@@ -1,33 +1,44 @@
-from typing import List
+def read_the_file(file_path: str) -> list:
+    try:
+        with open(file_path, 'r') as file:
+            data = file.readlines()
+
+        if not data:
+            print('File is empty.')
+        return data
+
+    except FileNotFoundError:
+        print('File not found.')
 
 
-def read_the_file(file_path: str):
-    with open(file_path, 'r') as file:
-        data = file.readlines()
-        if data:
-            print('file_content:')
-            for line in data:
-                process_the_content(line.strip())
-        else:
-            print('file_is_empty.')
+def process_the_content(lines: list) -> list[dict]:
+    results = []
+    for line in lines:
+        fields = line.strip().split(',')
+        username = fields[0]
+        scores = []
+        for score in fields:
+            try:
+                score_value = float(score.strip().replace(',', '', 1))
+                scores.append(score_value)
+            except ValueError:
+                pass
+
+        average = round(calculate_average(scores), 1)
+        results.append({'username': username, 'scores': scores, 'average': average})
+    return results
 
 
-def calculate_average(scores: List[float]):
-    if len(scores) > 0:
+def calculate_average(scores: list[float]) -> float:
+    if scores:
         return sum(scores) / len(scores)
     else:
         return 0
 
 
-def print_user_average_score(username: str, average: float) -> None:
-    print(f'User {username} has average: {average:.2f}')
-
-
-def process_the_content(line: str) -> None:
-    fields = line.strip().split(',')
-    username = fields[0]
-    scores = [float(score) for score in fields[1:]
-              if score.strip().replace(',', '', 1).isdigit()]
-    average = calculate_average(scores)
-
-    print_user_average_score(username, average)
+def print_results(results: list[dict]) -> None:
+    for result in results:
+        print('User:', result['username'])
+        print('Scores:', result['scores'])
+        print('Average:', result['average'])
+        print('_ _ _')
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # main.py
-from functions.methods import read_the_file
+from functions.methods import read_the_file, process_the_content, print_results
 
 
 def main():
     file_path = "resources/grades1.txt"
-    read_the_file(file_path)
+    lines = read_the_file(file_path)
+    results = process_the_content(lines)
+    print_results(results)
 
 
 if __name__ == "__main__":
