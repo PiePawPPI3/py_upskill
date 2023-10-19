540182244c5a39da625369654d33801b627c01ab
ppielka <ppielka@pgs-soft.com>
2023-10-19 13:58:47 +0200

    Sprint-3 class: Student

--- ports/student.py
+++ ports/student.py
@@ -1,21 +1,25 @@
+from typing import Optional
+
+
 class Student:
     def __init__(self, username: str, scores: list[float]):
         self._username = username
         self._scores = scores
-        self._average = None
+        self._average: Optional[float] = None
 
     def calculate_average(self) -> float:
-        if self._scores:
-            return round(sum(self._scores) / len(self._scores), 1)
-        else:
-            return 0.0
+        if self._average is None:
+            if self._scores:
+                average = round(sum(self._scores) / len(self._scores), 1)
+            else:
+                average = 0.0
+            self._average = average
+        return self._average
 
     def has_student_passed(self) -> bool:
+        average = self.calculate_average()
 
-        if self._average and self._average >= 2.0 and len(self._scores) >= 3:
-            return True
-        else:
-            return False
+        return average >= 2.0 and len(self._scores) >= 3
 
     def display_student_info(self):
         print('Username#: ' + self._username)
