a62c1d297a870b31914597b165b6e770d33775e4
ppielka <ppielka@pgs-soft.com>
2023-11-16 09:30:29 +0100

    Sprint-5 Venv_v1

---
+++ adapters/data_saver.py
@@ -0,0 +1,34 @@
+import datetime
+from abc import ABC
+
+from ports.data_parser import FileSaveError
+
+
+class FileWriter(ABC):
+
+    def write(self, content: str, output_path: str) -> None:
+        pass
+
+
+class TxtWriter(FileWriter):
+    def write(self, content: str, output_path: str) -> None:
+        try:
+            time_str = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+            file_path = f'{output_path}scores_{time_str}.txt'
+            with open(file_path, 'w') as file:
+                file.write(content)
+            print(f'Scores saved to "{file_path}" successfully.')
+        except Exception as e:
+            raise FileSaveError(e)
+
+
+class PdfWriter(FileWriter):
+    def write(self, content: str, output_path: str) -> None:
+        try:
+            time_str = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+            file_path = f'{output_path}scores_{time_str}.pdf'
+            with open(file_path, 'w') as file:
+                pass  # to:do
+            print(f'Scores saved to "{file_path}" successfully.')
+        except Exception as e:
+            raise FileSaveError(e)
