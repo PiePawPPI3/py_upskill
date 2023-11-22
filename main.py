9716ffb45960b5004150e82b20426587d05660b3
ppielka <ppielka@pgs-soft.com>
2023-11-22 22:11:41 +0100

    Sprint-5 refactor

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-# main.py
-from adapters.data_parser import read_scores, general_summary
-from adapters.data_saver import TxtWriter, PdfWriter
-from ports.data_parser import UnsupportedFileFormatError, FileNotFound, AccessDeniedError, BinaryFileError, \
-    EmptyFileError, InvalidGradeError, DataProcessingError, UnknownError,FileSaveError
+import datetime
+
+from adapters.data_parser import read_scores
+from adapters.data_saver import TxtWriter, PdfWriter, FileWriter, TerminalPrinter
+from ports.data_parser import (
+    UnsupportedFileFormatError, FileNotFound, AccessDeniedError, BinaryFileError,
+    EmptyFileError, InvalidGradeError, DataProcessingError, UnknownError, FileSaveError
+)
+from ports.student import Student
 
 
+def generate_summary_text(results: list[Student]) -> str:
+    summary = f'*** Created at: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}\n'
+    for student in results:
+        summary += student.get_student_info() + '\n'
+        summary += '_ _ _\n'
+    return summary
+
+
+def save_summary(summary: str, output_path: str, save_as_txt: bool, save_as_pdf: bool, display_summary: bool,
+                 txt_writer: FileWriter,
+                 pdf_writer: FileWriter, terminal_printer: FileWriter) -> None:
+    if save_as_txt:
+        txt_writer.write(summary, output_path)
+    if save_as_pdf:
+        pdf_writer.write(summary, output_path)
+    if display_summary:
+        terminal_printer.write(summary)
 
 
 def main():
@@ -17,9 +38,13 @@ def main():
 
     try:
         lines = read_scores(file_path)
+        summary_text = generate_summary_text(lines)
         txt_writer = TxtWriter()
         pdf_writer = PdfWriter()
-        general_summary(lines, output_path, save_as_txt, save_as_pdf, display_summary, txt_writer, pdf_writer)
+        terminal_printer = TerminalPrinter()
+
+        save_summary(summary_text, output_path, save_as_txt, save_as_pdf, display_summary, txt_writer, pdf_writer,
+                     terminal_printer)
 
     except FileNotFound:
         print('File not found.')
