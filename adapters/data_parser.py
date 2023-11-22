54c4b394757123812c103cd8e3043366d973c268
ppielka <ppielka@pgs-soft.com>
2023-11-22 22:11:25 +0100

    Sprint-5 refactor

--- adapters/data_parser.py
+++ adapters/data_parser.py
@@ -1,7 +1,4 @@
-import datetime
-from typing import Optional
 
-from adapters.data_saver import FileWriter
 from ports.data_parser import UnsupportedFileFormatError, FileNotFound, AccessDeniedError, BinaryFileError, \
     EmptyFileError, InvalidGradeError, DataProcessingError, UnknownError, FileSaveError
 from ports.student import Student
@@ -45,24 +42,3 @@ def read_scores(file_path: str) -> list[Student]:
         results.append(student)
 
     return results
-
-
-def general_summary(results: list[Student], output_path: str, save_as_txt: bool, save_as_pdf: bool,
-                    display_summary: bool, txt_writer: FileWriter, pdf_writer: FileWriter) -> Optional[str]:
-    summary = f'*** Created at: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}\n'
-    for student in results:
-        summary += student.display_student_info() + '\n'
-        summary += '_ _ _\n'
-
-    if save_as_txt:
-        txt_writer.write(summary, output_path)
-    if save_as_pdf:
-        pdf_writer.write(summary, output_path)
-    if display_summary:
-        print_to_terminal(summary)
-
-    return summary if (save_as_txt or save_as_pdf) else None
-
-
-def print_to_terminal(summary: Optional[str]) -> None:
-    print(summary)
