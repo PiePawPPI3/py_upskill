70077decd2d16d769b5207bbdb10bccd0270dcf2
PawełPie_ <118906707+PiePawPPI3@users.noreply.github.com>
2024-01-03 23:23:17 +0100

    Sprint 5 1510 venv (#4)
    * Sprint-4 Errors
    
    * Sprint-4 Errors_v2
    
    * Sprint-4 Errors_v3
    
    * Sprint-5 Venv_v1
    
    * Sprint-5 Venv_v1
    
    * Sprint-5 Venv_v1
    
    * Sprint-5_changes+add_outputs_folder
    
    * Sprint-5 refactor
    
    * Sprint-5 refactor
    
    * Sprint-4 Errors (#3)
    
    * Sprint-4 Errors
    
    * Sprint-4 Errors_v2
    
    * Sprint-4 Errors_v3
    
    * Sprint-4 Errors corr
    
    * Sprint-4 errors base (root) exception
    
    ---------
    
    Co-authored-by: ppielka <ppielka@pgs-soft.com>
    
    * Sprint-4 Errors
    
    * Sprint-4 Errors_v2
    
    * Sprint-4 Errors_v3
    
    * Sprint-5 Venv_v1
    
    * Sprint-5 Venv_v1
    
    * Sprint-5 Venv_v1
    
    * Sprint-5_changes+add_outputs_folder
    
    * Sprint-5 refactor
    
    * Sprint-5 refactor
    
    * Sprint-5 refactor main.py data_server.py
    
    * Sprint-5 dependencies file added requirements.txt
    
    * Sprint-5 jinja2
    
    * Sprint-5 jinja2 HtmlReport
    
    * Sprint-5 html and txt templates, refactor generate_summary
    
    * Sprint-5 html and txt templates, refactor generate_summary v2
    
    * Sprint-5 corrections
    
    ---------
    
    Co-authored-by: ppielka <ppielka@pgs-soft.com>

---
+++ adapters/data_saver.py
@@ -0,0 +1,69 @@
+import datetime
+from abc import ABC, abstractmethod
+
+from ports.data_parser import FileSaveError
+from jinja2 import Environment, FileSystemLoader
+
+from ports.student import Student
+
+
+class FileWriter(ABC):
+
+    @abstractmethod
+    def write(self, students_data: list[Student]) -> None:
+        pass
+
+
+class TxtWriter(FileWriter):
+    def __init__(self, output_path: str):
+        self.output_path = output_path
+
+    def write(self, students_data: list[Student]) -> None:
+
+        try:
+            time_str = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+            file_path = f'{self.output_path}scores_{time_str}.txt'
+
+            rendered_content = render_content(students_data, 'student_report_template.txt')
+
+            with open(file_path, 'w') as file:
+                file.write(rendered_content)
+            print(f'Scores saved to "{file_path}" successfully.')
+        except OSError as e:
+            raise FileSaveError(e)
+
+
+class HtmlWriter(FileWriter):
+
+    def __init__(self, output_path: str):
+        self.output_path = output_path
+
+    def write(self, students_data: list[Student]) -> None:
+        try:
+            time_str = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+            file_path = f'{self.output_path}scores_{time_str}.html'
+
+            rendered_content = render_content(students_data, 'student_report_template.html')
+
+            with open(file_path, 'w') as file:
+                file.write(rendered_content)
+            print(f'Scores saved to "{file_path}" successfully.')
+        except OSError as e:
+            raise FileSaveError(e)
+
+
+class TerminalPrinter(FileWriter):
+    def write(self, students_data: list[Student]) -> None:
+        rendered_content = render_content(students_data, 'student_report_template.txt')
+        print(rendered_content)
+
+
+def render_content(students_data: list[Student], template_name: str) -> str:
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template(template_name)
+
+    rendered_report = template.render({
+        'timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+        'students': students_data
+    })
+    return rendered_report
--- main.py
+++ main.py
@@ -1,15 +1,42 @@
-# main.py
-from adapters.data_parser import read_scores, display_scores
-from ports.data_parser import UnsupportedFileFormatError, FileNotFound, AccessDeniedError, BinaryFileError, \
-    EmptyFileError, InvalidGradeError, DataProcessingError, UnknownError
+from adapters.data_parser import read_scores
+from adapters.data_saver import TxtWriter, HtmlWriter, FileWriter, TerminalPrinter
+from ports.data_parser import (
+    UnsupportedFileFormatError, FileNotFound, AccessDeniedError, BinaryFileError,
+    EmptyFileError, InvalidGradeError, DataProcessingError, UnknownError, FileSaveError
+)
+from ports.student import Student
+
+
+def save_summary(summary: list[Student], writers: list[FileWriter]) -> None:
+    for writer in writers:
+        writer.write(summary)
 
 
 def main():
     file_path = "resources/grades1.txt"
+    output_path = "outputs/"
+
+    save_as_txt = True
+    save_as_html = True
+    display_summary = True
 
     try:
-        lines = read_scores(file_path)
-        display_scores(lines)
+        students = read_scores(file_path)
+
+        writers = []
+        if save_as_txt:
+            txt_writer = TxtWriter(output_path)
+            writers.append(txt_writer)
+
+        if save_as_html:
+            html_writer = HtmlWriter(output_path)
+            writers.append(html_writer)
+
+        if display_summary:
+            terminal_printer = TerminalPrinter()
+            writers.append(terminal_printer)
+
+        save_summary(students, writers)
 
     except FileNotFound:
         print('File not found.')
@@ -27,6 +54,8 @@ def main():
         print('Data processing error')
     except UnknownError:
         print('Unknown error encountered')
+    except FileSaveError as e:
+        print(f'Error occurred while saving the file: {e}')
 
 
 if __name__ == "__main__":
