c6de4c8085d3ff14130accb41d49a3a1d2c74650
ppielka <ppielka@pgs-soft.com>
2024-01-18 10:00:33 +0100

    Sprint pytest_test_terminal_printer-corr_1

--- tests/units/adapters/test_data_saver.py
+++ tests/units/adapters/test_data_saver.py
@@ -5,21 +5,24 @@ from ports.student import Student
 
 @pytest.fixture
 def students_data() -> list[Student]:
-    # students_data = [
-    #     Student('Romek', [4, 4, 5, 6]),
-    #     Student("Tomek", [3, 1, 5]),
-    #     Student("Atomek", [2, 5, 5, 6, 2]),
-    #     Student("Pawel", [1, 4, 4, 3]),
-    # ]
-    # return students_data
-    pass
+    students_data = [
+        Student('Romek', [4, 4, 5, 6]),
+        Student("Tomek", [3, 1, 5]),
+        Student("Atomek", [2, 5, 5, 6, 2]),
+        Student("Pawel", [1, 4, 4, 3]),
+    ]
+    return students_data
 
 
-def test_terminal_printer(mocker, students_data: list[Student], capsys) -> None:
-    mocker.patch("adapters.data_saver.render_content", return_value="zz")
+@pytest.fixture
+def terminal_printer():
+    return TerminalPrinter()
 
-    terminal_printer = TerminalPrinter()
+
+def test_terminal_printer(mocker, students_data: list[Student], capsys, terminal_printer) -> None:
+    mocker.patch("adapters.data_saver.render_content", return_value="Romek")
+
     terminal_printer.write(students_data)
 
     captured = capsys.readouterr()
-    assert "zz" in captured.out
+    assert "Romek" in captured.out
