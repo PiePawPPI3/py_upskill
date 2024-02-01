bc2938cc43f1001b86fcd908d4ed56e81189f7db
ppielka <ppielka@pgs-soft.com>
2024-02-01 11:56:06 +0100

    Sprint 6 UNIT TESTS

--- tests/units/adapters/test_data_saver.py
+++ tests/units/adapters/test_data_saver.py
@@ -1,15 +1,17 @@
 import pytest
-from adapters.data_saver import TerminalPrinter
+
+from adapters.data_saver import TerminalPrinter, TxtWriter
 from ports.student import Student
+from unittest.mock import Mock, call
 
 
 @pytest.fixture
 def students_data() -> list[Student]:
     students_data = [
         Student('Romek', [4, 4, 5, 6]),
-        Student("Tomek", [3, 1, 5]),
-        Student("Atomek", [2, 5, 5, 6, 2]),
-        Student("Pawel", [1, 4, 4, 3]),
+        Student('Tomek', [3, 1, 5]),
+        Student('Atomek', [2, 5, 5, 6, 2]),
+        Student('Pawel', [1, 4, 4, 3]),
     ]
     return students_data
 
@@ -19,10 +21,27 @@ def terminal_printer():
     return TerminalPrinter()
 
 
-def test_terminal_printer(mocker, students_data: list[Student], capsys, terminal_printer) -> None:
-    mocker.patch("adapters.data_saver.render_content", return_value="Romek")
+@pytest.fixture
+def txt_writer(tmpdir):
+    output_path = tmpdir.mkdir("output").strpath
+    return TxtWriter(output_path)
+
+
+def test_terminal_printer(mocker, students_data: list[Student], caplog, terminal_printer) -> None:
+    mocker.patch('adapters.data_saver.render_content', return_value='Romek')
 
     terminal_printer.write(students_data)
 
-    captured = capsys.readouterr()
-    assert "Romek" in captured.out
+    for record in caplog.records:
+        assert 'Romek' in record.message
+
+
+def test_txt_writer_writes_file(tmpdir, mocker, students_data: list[Student], txt_writer) -> None:
+    txt_writer.write(students_data)
+
+    time_str = mocker.datetime.now().strftime('%Y%m%d%H%M%S')
+    expected_file_path = tmpdir.join(f'scores_{time_str}.txt')
+
+    assert expected_file_path.exists()
+    content = expected_file_path.read_text()
+    assert 'Romek' in content
